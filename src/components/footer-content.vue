477074ffaab5d8a271ceb9fa002f0b91964ca851
hareeshcs33 <hareeshcs33@gmail.com>
2021-03-08 09:35:53 +0530

    footer data replaced static to dynomic

--- src/components/footer-content.vue
+++ src/components/footer-content.vue
@@ -2,29 +2,20 @@
   <div class="w-100 footer-content">
     <div class="container">
       <div
-        class="d-flex flex-column align-items-center justify-content-between p-5 mt-5"
+        class="d-flex flex-column align-items-center justify-content-between py-3 px-2 p-md-5 mt-5"
       >
         <section class="about-section" id="about">
           <h3 class="text-center">Why Choose Us?</h3>
           <p>
-            The supermarkets are crowded with all the brands and still if you
-            are looking for pure unprocessed honey, it is difficult to find. It
-            was this search for pure products away from the factory farms that
-            led us to start our store.
+            {{ whyChooseUsText[0].text }}
           </p>
           <p>
-            Today the world is crowded with lifestyle and food-related diseases.
-            Heart diseases, diabetes and cancer have become part of our lives.
-            What worries us is that these diseases have gradually crept into our
-            children as well. Obesity and early maturity in kids are growing.
-            Even the cases of diabetes and food allergy in kids are coming up.
-            With this store, we want to provide options to you to access good
-            food at the right price.
+            {{ whyChooseUsText[1].text }}
           </p>
         </section>
         <section class="contact-section w-100 mx-5 my-3 my-md-5" id="contact">
           <h3 class="text-center mb-3">Contact us</h3>
-          <div class="d-flex flex-column flex-md-row m-0">
+          <div class="d-flex flex-column flex-md-row align-items-md-center m-0">
             <div class="">
               <div class="img-box mx-auto mb-5 mb-md-0">
                 <img src="../assets/myc2pic.jpg" alt="image" class="item-img" />
@@ -32,9 +23,11 @@
             </div>
             <div class="m-0 ml-md-5">
               <p>Hareesh Sapparad</p>
-              <p>
+              <p class="d-flex align-items-center">
                 <i class="fas fa-envelope"></i
-                ><a class="link pl-2">hareesh_sapparad@gmail.com</a>
+                ><a class="link pl-2 ellipsis-email-text"
+                  >hareesh_sapparad@organicshopping.com</a
+                >
               </p>
               <p>
                 <i class="fas fa-phone-alt"></i>
@@ -66,15 +59,11 @@
           <div class="text-center">
             <div class="mb-2">
               &copy; 2021
-              <a class="link">hareesh_sapparad_organicshopping.com </a>
+              <a class="link">hareesh_sapparad@organicshopping.com </a>
               All rights reserved.
             </div>
             <p class="mb-0">
-              The content and images used on this site are copyright protected
-              and copyrights vests with the respective owners. The usage of the
-              content and images on this website is intended to promote the
-              works and no endorsement of the artist shall be implied.
-              Unauthorized use is prohibited and punishable by law.
+              {{ copyrightsText }}
             </p>
           </div>
         </footer>
@@ -83,7 +72,16 @@
   </div>
 </template>
 <script>
-export default {};
+import { whyChooseUsText, copyrightsText } from "../data/footer-data";
+
+export default {
+  data() {
+    return {
+      whyChooseUsText,
+      copyrightsText
+    };
+  }
+};
 </script>
 <style scoped>
 .footer-content {
@@ -99,5 +97,13 @@ export default {};
 }
 .img-box {
   border-radius: 100%;
+}
+@media (max-width: 480px) {
+  .ellipsis-email-text {
+    display: block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 }
 </style>
---
+++ src/data/footer-data.js
@@ -0,0 +1,8 @@
+export const whyChooseUsText = [{
+    "text": "The supermarkets are crowded with all the brands and still if you are looking for pure unprocessed honey, it is difficult to find. It was this search for pure products away from the factory farms that led us to start our store."
+  },
+  {
+    "text": "Today the world is crowded with lifestyle and food - related diseases.Heart diseases, diabetes and cancer have become part of our lives.What worries us is that these diseases have gradually crept into our children as well.Obesity and early maturity in kids are growing.Even the cases of diabetes and food allergy in kids are coming up.With this store, we want to provide options to you to access good food at the right price."
+  }
+];
+export const copyrightsText = "The content and images used on this site are copyright protected and copyrights vests with the respective owners. The usage of the content and images on this website is intended to promote the works and no endorsement of the artist shall be implied. Unauthorized use is prohibited and punishable by law.";
